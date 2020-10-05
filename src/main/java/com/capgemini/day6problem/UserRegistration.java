574e8b745b5d281f451144ebab560800fc67d9a7
Your Name <you@example.com>
2020-10-06 05:23:00 +0530

    [shashi] add . Validation Test Cases Added

--- src/main/java/com/capgemini/day6problem/UserRegistration.java
+++ src/main/java/com/capgemini/day6problem/UserRegistration.java
@@ -1,78 +1,30 @@
 package com.capgemini.day6problem;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.util.Scanner;
-
 public class UserRegistration {
-	private static final Logger LOG = LogManager.getLogger(UserRegistration.class);
-	private static String namePattern = "[A-Z]{1}[a-zA-Z]{2,}";
-	private static String emailPattern = "^(abc)([-/./+_]?[a-z0-9]+)?[a-z0-9]*[@][a-z1-9]+[/.][a-z]{2,3}([/.]?[a-z]{2,3})?$";
-	private static final String mobilePattern = "[0-9]{2}[' ']{1}[789]{1}[0-9]{9}";
+
+	private static final String namePattern = "^[A-Z]{1}[a-zA-Z]{2,}";
+	private static final String emailPattern = "^[0-9a-zA-Z]+[_+-.]{0,1}[a-zA-Z0-9]+@[a-zA-Z0-9]+[.]{1}[a-zA-Z]{2,4}([.]{0,1}[a-z]{2,4}){0,1}$";
+	private static final String mobilePattern = "[1-9]{2}[' ']{1}[789]{1}[0-9]{9}";
 	private static final String passwordPattern = "^(?=.{8,}$)(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]*[@#$%^*!&][0-9a-zA-Z]*";
-			
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		UserRegistration userRegistration = new UserRegistration();
-		String first, last;
-		LOG.info("Enter your first name: ");
-		first = sc.nextLine();
-		if (userRegistration.validateFirstName(first))
-			LOG.info(" Valid first name!");
-		else
-			LOG.info(" Invalid first name!");
 
-		LOG.info("\nEnter your last name: ");
-		last = sc.nextLine();
-
-		if (userRegistration.validateFirstName(last))
-			LOG.info(" Valid last name!");
-		else
-			LOG.info(" Invalid last name!");
-
-		LOG.info("Enter your email id: ");
-		String[] sampleIds = new String[] { "abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com",
-				"abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com", "abc",
-				"abc@.com.my", "abc123@gmail.a", "abc123@.com", "abc123@.com.com", ".abc@abc.com", "abc()*@gmail.com",
-				"abc@%*.com", "abc..2002@gmail.com", "abc.@gmail.com", "abc@abc@gmail.com", "abc@gmail.com.1a",
-				"abc@gmail.com.aa.au" };
-		for (String sample : sampleIds) {
-			LOG.info(sample + ": " + validateEmailSamples(sample));
-		}
-		LOG.info("Enter your mobile number with country code: ");
-		String mobileNumber = sc.nextLine();
-		if (userRegistration.validateMobileNumber(mobileNumber))
-			LOG.info(" Valid mobile number!");
-		else
-			LOG.info(" Invalid mobile number!");
-		
-		LOG.info("Enter your password having minimum 8 characters with atleast 1 Upper case, 1 digit and only 1 special character: ");
-		String password = sc.nextLine();
-		if (userRegistration.validatePassword(password))
-			LOG.info(" Valid password!");
-		else
-			LOG.info(" Invalid password!");
-		sc.close();
+	public boolean validateFirst(String fname) {
+		return fname.matches(namePattern);
 	}
 
-	public boolean validateFirstName(String first) {
-		return first.matches(namePattern);
+	public boolean validateLast(String lname) {
+		return lname.matches(namePattern);
 	}
 
-	public boolean validateLastName(String last) {
-		return last.matches(namePattern);
-	}
-
-	public static boolean validateEmailSamples(String email) {
+	public boolean validateEmail(String email) {
 		return email.matches(emailPattern);
 	}
-	
+
 	public boolean validateMobileNumber(String mobile) {
 		return mobile.matches(mobilePattern);
 	}
-	
+
 	public boolean validatePassword(String password) {
 		return password.matches(passwordPattern);
 	}
+
 }
--- src/test/java/com/capgemini/day6problem/UserRegistrationTest.java
+++ src/test/java/com/capgemini/day6problem/UserRegistrationTest.java
@@ -1,14 +1,155 @@
 package com.capgemini.day6problem;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 public class UserRegistrationTest {
 
 	@Test
-	public void whenProper_ShouldReturnTrue() {
-		assertTrue(true);
+	public void whenGiven_ProperFirstName_ShouldReturn_True() {
+		UserRegistration userRegistration = new UserRegistration();
+		boolean result = userRegistration.validateFirst("Shashi");
+		Assert.assertTrue(result);
+	}
+
+	@Test
+	public void whenGiven_ShortFirstName_ShouldReturn_False() {
+		UserRegistration userRegistration = new UserRegistration();
+		boolean result = userRegistration.validateFirst("Sh");
+		Assert.assertFalse(result);
+	}
+
+	@Test
+	public void whenGiven_NotLetterFirst_ShouldReturn_False() {
+		UserRegistration userRegistration = new UserRegistration();
+		boolean result = userRegistration.validateFirst("Shashi*");
+		Assert.assertFalse(result);
+	}
+
+	@Test
+	public void whenGiven_NotCapitalFirst_ShouldReturn_False() {
+		UserRegistration userRegistration = new UserRegistration();
+		boolean result = userRegistration.validateFirst("shashi");
+		Assert.assertFalse(result);
+	}
+
+	@Test
+	public void whenGiven_ProperLastName_ShouldReturn_True() {
+		UserRegistration userRegistration = new UserRegistration();
+		boolean result = userRegistration.validateLast("Bhushan");
+		Assert.assertTrue(result);
+	}
+
+	@Test
+	public void whenGiven_ShortName_ShouldReturn_False() {
+		UserRegistration userRegistration = new UserRegistration();
+		boolean result = userRegistration.validateLast("Bh");
+		Assert.assertFalse(result);
+	}
+
+	@Test
+	public void whenGiven_NotLetterLast_ShouldReturn_False() {
+		UserRegistration userRegistration = new UserRegistration();
+		boolean result = userRegistration.validateLast("bhushan55");
+		Assert.assertFalse(result);
+	}
+
+	@Test
+	public void whenGiven_NotCapitalLast_ShouldReturn_False() {
+		UserRegistration userRegistration = new UserRegistration();
+		boolean result = userRegistration.validateLast("chadnel");
+		Assert.assertFalse(result);
+	}
+
+	@Test
+	public void whenGiven_ProperEmail_ShouldReturn_True() {
+		UserRegistration userRegistration = new UserRegistration();
+		boolean result = userRegistration.validateEmail("abc.100@bl.co.in");
+		Assert.assertTrue(result);
+	}
+
+	@Test
+	public void whenGiven_ImProperEmail_ShouldReturn_False() {
+		UserRegistration userRegistration = new UserRegistration();
+		boolean result = userRegistration.validateEmail("abc.@gmail.com");
+		Assert.assertFalse(result);
+	}
+
+	@Test
+	public void whenGiven_ProperMobile_ShouldReturn_True() {
+		UserRegistration userRegistration = new UserRegistration();
+		boolean result = userRegistration.validateMobileNumber("91 9875858585");
+		Assert.assertTrue(result);
+	}
+
+	@Test
+	public void whenGiven_LessDigits_ShouldReturn_False() {
+		UserRegistration userRegistration = new UserRegistration();
+		boolean result = userRegistration.validateMobileNumber("91 89874");
+		Assert.assertFalse(result);
+	}
+
+	@Test
+	public void whenGiven_ImproperCountryCode_ShouldReturn_False() {
+		UserRegistration userRegistration = new UserRegistration();
+		boolean result = userRegistration.validateMobileNumber("00 8987489898");
+		Assert.assertFalse(result);
+	}
+
+	@Test
+	public void whenGiven_WithoutCountryCode_ShouldReturn_False() {
+		UserRegistration userRegistration = new UserRegistration();
+		boolean result = userRegistration.validateMobileNumber("8987489898");
+		Assert.assertFalse(result);
+	}
+
+	@Test
+	public void whenGiven_MoreThanTenDigits_ShouldReturn_False() {
+		UserRegistration userRegistration = new UserRegistration();
+		boolean result = userRegistration.validateMobileNumber("91 89898989889");
+		Assert.assertFalse(result);
+	}
+
+	@Test
+	public void whenGiven_ProperPassword_ShouldReturn_True() {
+		UserRegistration userRegistration = new UserRegistration();
+		boolean result = userRegistration.validatePassword("Rahul9@2");
+		Assert.assertTrue(result);
+	}
+
+	@Test
+	public void whenGiven_LessThanEightDigits_ShouldReturn_False() {
+		UserRegistration userRegistration = new UserRegistration();
+		boolean result = userRegistration.validatePassword("kumr56!@");
+		Assert.assertFalse(result);
+	}
+
+	@Test
+	public void whenGiven_WithoutCapitalLetter_ShouldReturn_False() {
+		UserRegistration userRegistration = new UserRegistration();
+		boolean result = userRegistration.validatePassword("rahul8548_");
+		Assert.assertFalse(result);
+	}
+
+	@Test
+	public void whenGiven_WithoutNumber_ShouldReturn_False() {
+		UserRegistration userRegistration = new UserRegistration();
+		boolean result = userRegistration.validatePassword("Kumar!!6");
+		Assert.assertFalse(result);
+	}
+
+	@Test
+	public void whenGiven_WithoutSpecialCharater_ShouldReturn_False() {
+		UserRegistration userRegistration = new UserRegistration();
+		boolean result = userRegistration.validatePassword("Shaty80");
+		Assert.assertFalse(result);
+	}
+
+	@Test
+	public void whenGiven_MoreThanOneSpecialCharater_ShouldReturn_False() {
+		UserRegistration userRegistration = new UserRegistration();
+		boolean result = userRegistration.validatePassword("@@shSi20");
+		Assert.assertFalse(result);
 	}
 
 }
