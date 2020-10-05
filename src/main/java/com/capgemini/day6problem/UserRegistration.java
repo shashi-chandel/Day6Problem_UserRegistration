17720316734ef4edf44bec073bb6b503739de7c3
Your Name <you@example.com>
2020-10-05 19:01:25 +0530

    [shashi] add . First Name Validation checked

--- src/main/java/com/capgemini/day6problem/UserRegistration.java
+++ src/main/java/com/capgemini/day6problem/UserRegistration.java
@@ -3,11 +3,23 @@ package com.capgemini.day6problem;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
 	private static final Logger LOG = LogManager.getLogger(UserRegistration.class);
-
+	private static String namePattern = "[A-Z]{1}[a-zA-Z]{2,}";
 	public static void main(String[] args) {
-		String message = "Welcome to User Registration Form!!!";
-		LOG.info("{}",message);
+		Scanner sc = new Scanner(System.in);
+		LOG.info("Enter your First Name:");
+		String first = sc.nextLine();
+		if (Pattern.matches(namePattern, first))
+			LOG.info("Valid first name");
+		else
+			LOG.info("Invalid entry!!");
+	}
+	
+	public boolean validateFirstName(String first) {
+		return first.matches(namePattern);
 	}
 }
---
+++ src/test/java/com/capgemini/day6problem/UserRegistrationTest.java
@@ -0,0 +1,14 @@
+package com.capgemini.day6problem;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserRegistrationTest {
+
+	@Test
+	public void whenProper_ShouldReturnTrue() {
+		assertTrue(true);
+	}
+
+}
