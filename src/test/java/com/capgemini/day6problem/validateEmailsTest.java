97b1c804a280d8aa9b7028161d425c8bfd17f7a2
Your Name <you@example.com>
2020-10-06 05:39:57 +0530

    [shashi] add . Multiple Validation Checked

---
+++ src/test/java/com/capgemini/day6problem/validateEmailsTest.java
@@ -0,0 +1,40 @@
+package com.capgemini.day6problem;
+
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class validateEmailsTest {
+	private String email;
+	private boolean result;
+
+	public validateEmailsTest(String email, boolean result) {
+		super();
+		this.email = email;
+		this.result = result;
+	}
+
+	@Parameterized.Parameters
+	public static Collection input() {
+		return Arrays.asList(new Object[][] { { "abc@yahoo.com", true }, { "abc-100@yahoo.com", true },
+				{ "abc.100@yahoo.com", true }, { "abc111@abc.com", true }, { "abc-100@abc.net", true },
+				{ "abc.100@abc.com.au", true }, { "abc@1.com", true }, { "abc@gmail.com.com", true },
+				{ "abc+100@gmail.com", true }, { "abc", false }, { "abc@.com.my", false }, { "abc123@gmail.a", false },
+				{ "abc123@.com", false }, { "abc123@.com.com", false }, { ".abc@abc.com", false },
+				{ "abc()*@gmail.com", false }, { "abc@%*.com", false }, { "abc..2002@gmail.com", false },
+				{ "abc.@gmail.com", false }, { "abc@abc@gmail.com", false }, { "abc@gmail.com.1a", false },
+				{ "abc@gmail.com.aa.au", false } });
+	}
+
+	@Test
+	public void givenEmail_ShouldReturnResult() {
+		UserRegistration userRegistration = new UserRegistration();
+		boolean result = userRegistration.validateEmail(this.email);
+		Assert.assertEquals(this.result, result);
+	}
+
+}
